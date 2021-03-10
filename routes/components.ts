54e63d95497d54129443d27142c50b458e3e9e84
amltms <amalthomas587@gmail.com>
2021-03-10 18:28:50 +0000

    done update and delete; crud complete

---
+++ routes/components.ts
@@ -0,0 +1,14 @@
+import {Router}  from "https://deno.land/x/oak/mod.ts";
+import {getComponents, getComponent, addComponent, updateComponent, deleteComponent} from '../controllers/components.ts';
+
+const router = new Router();
+
+
+  router
+    .get("/component", getComponents)
+    .post("/component", addComponent)
+    .get("/component/:id", getComponent)
+    .put("/component/:id", updateComponent)
+    .delete("/component/:id", deleteComponent);
+
+export default router;
